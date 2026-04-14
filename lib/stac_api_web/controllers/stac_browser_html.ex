d02b0fc6495b7a35ea9a9399f6ac328c7dfe3a4d
Muhammad Sudais Akbar <msudaisakbar434@gmail.com>
2026-04-14 21:23:18 +0300

    feat: add MapLibre views for collection extent and item geometry

--- lib/stac_api_web/controllers/stac_browser_html.ex
+++ lib/stac_api_web/controllers/stac_browser_html.ex
@@ -4,4 +4,77 @@ defmodule StacApiWeb.StacBrowserHTML do
   import StacApiWeb.StacBrowserHelpers
 
   embed_templates "stac_browser_html/*"
+
+  # Map component for displaying collection extent and item geometry
+  def stac_map(assigns) do
+    geojson_data =
+      if assigns[:geometry] && assigns[:properties] do
+        geometry = convert_geography_to_geojson(assigns[:geometry])
+        %{"type" => "Feature", "geometry" => geometry, "properties" => assigns[:properties]}
+      else
+        extent = assigns[:geojson_data] || %{}
+        convert_extent_to_geojson(extent)
+      end
+
+    assigns = assign(assigns, :geojson_data, geojson_data)
+
+    ~H"""
+    <div
+      id={@map_id}
+      class="w-full h-96 rounded-lg border border-gray-300 shadow-sm"
+      data-map-id={@map_id}
+      data-geojson={Jason.encode!(@geojson_data)}
+    >
+    </div>
+    """
+  end
+
+  # Convert PostGIS Geo.* objects to GeoJSON format
+  defp convert_geography_to_geojson(%Geo.Polygon{coordinates: coords}) do
+    # Convert tuples to lists recursively
+    coordinates = convert_tuples_to_lists(coords)
+    %{"type" => "Polygon", "coordinates" => coordinates}
+  end
+
+  defp convert_geography_to_geojson(%Geo.Point{coordinates: {lon, lat}}) do
+    %{"type" => "Point", "coordinates" => [lon, lat]}
+  end
+
+  defp convert_geography_to_geojson(geom) when is_map(geom) do
+    geom
+  end
+
+  defp convert_geography_to_geojson(geom), do: geom
+
+  # Recursively convert tuples to lists for JSON encoding
+  defp convert_tuples_to_lists(data) when is_tuple(data) do
+    data |> Tuple.to_list() |> convert_tuples_to_lists()
+  end
+
+  defp convert_tuples_to_lists(data) when is_list(data) do
+    Enum.map(data, &convert_tuples_to_lists/1)
+  end
+
+  defp convert_tuples_to_lists(data), do: data
+
+  # Convert extent JSON to GeoJSON feature
+  defp convert_extent_to_geojson(%{"spatial" => %{"bbox" => [bbox_list | _]}} = _extent) do
+    [min_lon, min_lat, max_lon, max_lat] = bbox_list
+    %{
+      "type" => "Feature",
+      "geometry" => %{
+        "type" => "Polygon",
+        "coordinates" => [[
+          [min_lon, min_lat],
+          [max_lon, min_lat],
+          [max_lon, max_lat],
+          [min_lon, max_lat],
+          [min_lon, min_lat]
+        ]]
+      }
+    }
+  end
+
+  defp convert_extent_to_geojson(extent) when is_map(extent), do: extent
+  defp convert_extent_to_geojson(_), do: %{}
 end
