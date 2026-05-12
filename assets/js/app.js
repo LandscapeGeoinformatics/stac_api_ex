cf38060a4adf717d872c73c2b9c1cbf4b0712b50
Alexander Kmoch <alexander.kmoch@ut.ee>
2026-05-13 00:49:59 +0300

    css and bundling all the way

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,6 @@
-import '../css/app.css';
-import 'maplibre-gl/dist/maplibre-gl.css';
 import MaplibreMapHook from './maplibre_hook.js';
-// Initialize maps on page load
 import { initStacMaps } from './maplibre_hook.js';
+
 window.Hooks = {
   MaplibreMapHook
 };
