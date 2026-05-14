dc276474a1d5230a14972b5fd2c84c3531776cf5
Ats Remmelg <ats.remmelg@ut.ee>
2026-05-14 10:25:43 +0300

    Style updates - updated the landing page - updated the tailwind config to include DataCube colors 	#	modified:   lib/stac_api_web/components/layouts/root.html.heex

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -4,5 +4,17 @@ module.exports = {
     "../lib/**/*.heex",
     "../lib/**/*.ex"
   ],
-  plugins: [require("daisyui")]
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#D5DF2E",
+          secondary: "#143F72",
+          accent: "#22c55e",
+          neutral: "#1f2937"
+        },
+      },
+    ],
+  },
 }
